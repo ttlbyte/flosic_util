92fbc9a30c905b6f6043d024abb15f8362d92733
Shiqi Ruan <shiqi.ruan@temple.edu>
2022-06-13 14:53:46 -0400

    update cluster_from_xyz

--- cluster_from_xyz.py
+++ cluster_from_xyz.py
@@ -5,11 +5,11 @@ from ase.io import read
 import sys
 import os
 
-##  Usage: ./cluster_from_xyz.py [filename] [method] [charge] [spin]
+# Usage: ./cluster_from_xyz.py [filename] [method] [charge] [spin]
 
-n2b=1.889726
+n2b = 1.889726
 if len(sys.argv) > 1:
-    atoms=read(sys.argv[1])
+    atoms = read(sys.argv[1])
 else:
     for i in os.listdir():
         if 'xyz' in i:
@@ -19,7 +19,7 @@ if len(sys.argv) >= 3:
     method = argv[2]
 else:
     method = "LDA-PW91"
-if len(sys.argv) >=5:
+if len(sys.argv) >= 5:
     charge = float(sys.argv[3])
     spin = float(sys.argv[4])
 else:
@@ -27,9 +27,12 @@ else:
     spin = 0.0
 
 with open('CLUSTER', 'w') as f:
-    f.write("{}*{}            (DF TYPE EXCHANGE*CORRELATION)\n".format(method,method))
+    f.write("{}*{}            (DF TYPE EXCHANGE*CORRELATION)\n".format(method, method))
     f.write("NONE                         (TD, OH, IH, X, Y, XY, ... OR GRP)\n")
     f.write("{}\n".format(len(atoms)))
     for i in atoms:
-        f.write("{:14.8f}  {:14.8f}  {:14.8f}   {:2d}  ALL (R, Z, ALL-ELECTRON)\n".format((i.position*n2b), i.number))
-    f.write("{:.2f} {:.2f}                      (NET CHARGE AND NET SPIN)".format(charge, spin))
+        [a, b, c] = i.position*n2b
+        f.write(
+            "{:14.8f}  {:14.8f}  {:14.8f}   {:2d}  ALL (R, Z, ALL-ELECTRON)\n".format(a, b, c, i.number))
+    f.write("{:.2f} {:.2f}                      (NET CHARGE AND NET SPIN)".format(
+        charge, spin))
